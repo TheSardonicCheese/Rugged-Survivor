ce755489d238fc74805b4889fc72b523bd318423
LukeStanley <1015748@student.sae.edu.au>
2020-03-20 12:58:06 +1100

    Click to Move very basic

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int hunger = 1;
+    public int thirst = 1;
+    public int muscles;
+    public int wakefulness;
+    public int speed;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = hunger + thirst * muscles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
