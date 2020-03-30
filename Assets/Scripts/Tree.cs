5b159a839cbfd5e0cc24b5195adb1a5e0ab349fb
LukeStanley <1015748@student.sae.edu.au>
2020-03-30 15:46:03 +1100

    Punch trees with space to get wood

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public int health;// Start is called before the first frame update
+    void Start()
+    {
+        health = Random.Range(2, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
