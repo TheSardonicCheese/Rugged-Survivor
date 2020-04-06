5a25682319274fea875f17f02587b962e3ea37f9
PyrotechnicCake <onetruejake@outlook.com>
2020-04-06 15:26:12 +1000

    made food edible and deduct from inventory when eaten

--- Assets/Scripts/EatMeat.cs
+++ Assets/Scripts/EatMeat.cs
@@ -24,10 +24,40 @@ public class EatMeat : MonoBehaviour
 
     public void EatFood()
     {
-        print("you ate a " + itemName);
-        //recover stats
-        myplayer.GetComponent<PlayerStats>().health += healthImpact;
-        myplayer.GetComponent<PlayerStats>().hunger += saturation;
-        myplayer.GetComponent<PlayerStats>().thirst += hydration;
+        if (itemName == "apple" && myplayer.GetComponent<PlayerStats>().apples >= 1)
+        {
+            print("you ate an " + itemName);
+            //recover stats
+            myplayer.GetComponent<PlayerStats>().health += healthImpact;
+            myplayer.GetComponent<PlayerStats>().hunger += saturation;
+            myplayer.GetComponent<PlayerStats>().thirst += hydration;
+            //remove apple
+            myplayer.GetComponent<PlayerStats>().apples -= 1;
+
+        }
+        else if (itemName == "meat" && myplayer.GetComponent<PlayerStats>().meat >= 1)
+        {
+            print("you ate some " + itemName);
+            //recover stats
+            myplayer.GetComponent<PlayerStats>().health += healthImpact;
+            myplayer.GetComponent<PlayerStats>().hunger += saturation;
+            myplayer.GetComponent<PlayerStats>().thirst += hydration;
+            //remove meat
+            myplayer.GetComponent<PlayerStats>().meat -= 1;
+        }
+        else if (itemName == "steak" && myplayer.GetComponent<PlayerStats>().steak >= 1)
+        {
+            print("you ate some " + itemName);
+            //recover stats
+            myplayer.GetComponent<PlayerStats>().health += healthImpact;
+            myplayer.GetComponent<PlayerStats>().hunger += saturation;
+            myplayer.GetComponent<PlayerStats>().thirst += hydration;
+            //increase muscle charge
+            myplayer.GetComponent<PlayerStats>().muscleCharge += 1;
+            //remove meat
+            myplayer.GetComponent<PlayerStats>().steak -= 1;
+        }
+
+
     }
 }
